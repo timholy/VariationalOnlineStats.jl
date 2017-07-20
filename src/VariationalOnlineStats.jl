392ed3a5f06597d768c7c8f7b47be7fe4c66d34a
Tim Holy <tim.holy@gmail.com>
2017-07-20 13:39:59 -0500

    Add online median

--- src/VariationalOnlineStats.jl
+++ src/VariationalOnlineStats.jl
@@ -1,5 +1,42 @@
 module VariationalOnlineStats
 
-# package code goes here
+using Base.Iterators
+
+export MedianOnline
+
+struct MedianOnline{T}
+    med::T   # median
+    mad::T   # mean absolute deviation
+    n::Int
+end
+(::Type{MedianOnline{T}}){T}(x) = MedianOnline{T}(x, zero(T), 1)
+
+MedianOnline(x::T) where {T<:AbstractFloat} = MedianOnline{T}(x)
+
+function MedianOnline{T}(v::AbstractVector{<:Number}) where {T<:AbstractFloat}
+    m = MedianOnline{T}(first(v))
+    for x in drop(v, 1)
+        m = median(m, x)
+    end
+    m
+end
+MedianOnline(v::AbstractVector{T}) where {T<:AbstractFloat} = MedianOnline{T}(v)
+
+@inline function Base.median(m::MedianOnline{T}, x) where {T}
+    f = 1/(m.n + 1)  # fraction of contribution to mad
+    Δx = x - m.med
+    aΔx = abs(Δx)
+    mad = (1-f) * m.mad + f * aΔx
+    Δv = mad/m.n   # separation of the closest point among n from an
+                   # exponential distribution with the given mad
+    med = m.med + sign(Δx) * min(aΔx, Δv)
+    MedianOnline{T}(med, mad, m.n+1)
+end
+
+
+
+Base.convert(::Type{T}, m::MedianOnline{T}) where {T<:Number} = m.med
+Base.convert(::Type{T}, m::MedianOnline{T}) where T = m.med
+Base.convert(::Type{T}, m::MedianOnline{<:Number}) where {T<:Number} = convert(T, m.med)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,19 @@
 using VariationalOnlineStats
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+for n = 1:2
+    for i = 1:100
+        x = rand(n)
+        @test Float64(MedianOnline(x)) ≈ median(x)
+    end
+end
+
+@test Float64(MedianOnline(Float64[1, 3, 2])) ≈ 2.0
+@test Float64(MedianOnline(Float64[3, 1, 2])) ≈ 2.0
+@test abs(Float64(MedianOnline(Float64[1, 2, 3])) - 2) < 0.1
+@test abs(Float64(MedianOnline(Float64[3, 2, 1])) - 2) < 0.1
+
+v = [1, 11, 10, 2, 3, 9, 8, 4, 5, 7, 6]
+@test abs(Float64(MedianOnline{Float64}(v)) - 6) < 0.1
+@test Float64(MedianOnline{Float64}(1:11)) > 4.5
+@test Float64(MedianOnline{Float64}(11:-1:1)) < 7.5
